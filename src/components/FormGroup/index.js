ef4d09463bd19c1b54dec7dc2d60bd21c3481557
pedrocleal <pedroxleal10@gmail.com>
2022-07-05 16:55:08 -0300

    :fire: refactor: spinner component

--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 
+import Spinner from '../Spinner';
+
 import { Container } from './styles';
 
 export default function FormGroup({ children, error, isLoading }) {
@@ -9,7 +11,11 @@ export default function FormGroup({ children, error, isLoading }) {
       <div className="form-item">
         {children}
 
-        {isLoading && <div className="loader" />}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
       {error && <small>{error}</small>}
     </Container>
